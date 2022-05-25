1f5dd856916066eca9ba9a298bb66db0534f94c0
Dragonlosers1230 <lucas.driessens@student.howest.be>
2022-05-25 10:16:13 +0200

    fix: historiek -> logs

--- backend/repositories/DataRepository.py
+++ backend/repositories/DataRepository.py
@@ -18,30 +18,30 @@ class DataRepository:
 
     @staticmethod
     def get_historiek():
-        sql = "select * from historiek order by actiedatum desc"
+        sql = "select * from logs order by actiedatum desc"
         return Database.get_rows(sql)
 
     @staticmethod
     def get_specific_historiek(id):
-        sql = "select * from historiek where volgnummer = %s"
+        sql = "select * from logs where volgnummer = %s"
         params = [id]
         return Database.get_one_row(sql,params)
 
     @staticmethod
     def get_latest_value(id):
-        sql = "select Waarde from historiek where deviceID = %s ORDER BY volgnummer DESC LIMIT 1"
+        sql = "select Waarde from logs where deviceID = %s ORDER BY volgnummer DESC LIMIT 1"
         params = [id]
         return Database.get_one_row(sql,params)
 
     @staticmethod
     def update_waterlevel(value,deviceid,actieid):
-        sql = "INSERT INTO historiek(waarde,deviceID,actieID) VALUES(%s,%s,%s)"
+        sql = "INSERT INTO logs(waarde,deviceID,actieID) VALUES(%s,%s,%s)"
         params = [value,deviceid,actieid]
         return Database.execute_sql(sql,params)
 
     @staticmethod
     def delete_readings_today(datum):
-        sql = "delete from historiek where actiedatum like %s"
+        sql = "delete from logs where actiedatum like %s"
         params = [str(datum)+"%"]
         return Database.execute_sql(sql,params)
         
